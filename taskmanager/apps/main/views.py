1f9ea82b51294bb18e25b3b38b961252e75f5fc6
Ilya <107436210+KamenikII@users.noreply.github.com>
2025-05-23 11:25:55 +0300

    Add registration, login and logout functional

--- taskmanager/apps/main/views.py
+++ taskmanager/apps/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Task
-from .forms import TaskForm
-
+from .forms import TaskForm, RegisterUserForm
+from django.contrib.auth import login
 
 def index(request):
     tasks = Task.objects.all()
@@ -12,6 +13,7 @@ def about(request):
     return render(request, 'main/about.html')
 
 
+@login_required
 def create(request):
     error = ''
     if request.method == 'POST':
@@ -30,6 +32,7 @@ def create(request):
     return render(request, 'main/create.html', context)
 
 
+@login_required
 def update(request, id):
     task = get_object_or_404(Task, id=id)
     if request.method == 'POST':
@@ -42,7 +45,20 @@ def update(request, id):
     return render(request, 'main/create.html', {'form': form, 'error': '', 'title': 'Edit Task'})
 
 
+@login_required
 def delete(request, id):
     task = get_object_or_404(Task, id=id)
     task.delete()
     return redirect('homeless')
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # автоматически логинит после регистрации
+            return redirect('homeless')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'registration/register.html', {'form': form})
